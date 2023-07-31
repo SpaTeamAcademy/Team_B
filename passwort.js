a2cf05e607f8da33da5a0a08ce66fd352b69bc78
Gabriel Horn <>
2023-07-31 14:09:13 +0200

    Die gewünschten changes sind vorgenommen worden.

--- passwort.js
+++ passwort.js
@@ -1,20 +1,12 @@
 function generatePasswort() {
     const kleinbuchstaben = "abcdefghijklmnopqrstuvwxyz";
-    /* const Großbuchstaben = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const Zahlen = "0123456789";
-    const Sonderzeichen = "!@#$%^&*()-_=+[]{}|;:,.<>?";
-    */
+
    var Länge = 10;
    let passwort = "";
 
-   passwort += getRandomChar(kleinbuchstaben);
 
    for (let i = passwort.length; i < Länge; i++) {
-    const charType = Math.floor(Math.random() * 0);
-    switch (charType) {
-        case 0:
         passwort += getRandomChar(kleinbuchstaben); 
-    }
 
    }
    document.getElementById("generatedpassword").innerText = "Dein Passwort ist = " + passwort
