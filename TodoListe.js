74a45a6ef0d463a738543672fbb73b47c55389a7
Katrin Hinterberger <k.hinterberger@outlook.com>
2023-08-01 13:14:21 +0200

    initial commit

--- TodoListe.js
+++ TodoListe.js
@@ -1,12 +1,25 @@
 function addItem() {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === "") {
-      alert("Bitte gib etwas ein :)");
-    } else {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+
+  li.appendChild(t);
+
+  if (inputValue === "") {
+      alert("Bitte gib etwas ein :)")
+    } 
+    else {
+      var deleteBtn = document.createElement("span");
+      deleteBtn.innerHTML = "\u00D7";
+      deleteBtn.classList.add("delete-btn");
+
+      deleteBtn.addEventListener("click", function () {
+          this.parentElement.remove();
+      });
+
+      li.appendChild(deleteBtn);
       document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
+  }
+
+  document.getElementById("myInput").value = "";
 }
